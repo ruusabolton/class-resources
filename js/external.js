c39cfecba9640f32a2715c567b6be2cdda8794e2
Ruusa Bolton <ruusabolton@Ruusas-MacBook-Pro.local>
2018-06-16 15:14:37 -0500

    testing ...

---
+++ js/external.js
@@ -0,0 +1,23 @@
+'use strict';
+
+
+
+
+var hello;
+
+console.log('hello');
+
+var age = 27;
+console.log('age');
+
+var myAge = 25;
+
+var confirmed = confirm;
+console.log(confirmed);
+
+
+var answer = confirm('Is it your b-day today?');
+console.log('answer');
+
+var userInput = prompt('Please type something: ');
+console.log('The user entered: ' + userInput);
